0bd8393d6616dc8263ae3baaf06fece4e698c051
Camila <camilamatencio@gmail.com>
2020-06-09 15:37:42 -0300

    add 4 gallery

--- scripts.js
+++ scripts.js
@@ -93,3 +93,34 @@ for (i = 0; i < x.length; i++) {
 x[slideIndex-1].style.display = "block";  
 }
 /* ----3---- */
+/* galeria 4 */
+function myFunctionFour() {
+    document.getElementById("galeria-cuatro").style.cssText  = "display: flex;";
+    document.getElementById("video").style.cssText  = "display: none;";
+    document.getElementById("first-image").style.cssText  = "margin-top: 50px;";
+}
+
+function closeWindowFour() {
+    document.getElementById("galeria-cuatro").style.display = "none";
+    document.getElementById("video").style.cssText  = "display: block;";
+    document.getElementById("first-image").style.cssText  = "margin-top: 25px;";
+}
+
+var slideIndex = 1;
+showDivsFour(slideIndex);
+
+function plusDivsFour(n) {
+showDivsFour(slideIndex += n);
+}
+
+function showDivsFour(n) {
+var i;
+var x = document.getElementsByClassName("mySlidesFour");
+if (n > x.length) {slideIndex = 1}
+if (n < 1) {slideIndex = x.length}
+for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+}
+x[slideIndex-1].style.display = "block";  
+}
+/* ----4---- */
